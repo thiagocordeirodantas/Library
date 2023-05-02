92324b5971c9c04f4534adc041f28088f2649ab2
Thiago Cordeiro Dantas <thiagoccordeirodantas@gmail.com>
2023-05-01 21:38:00 -0300

    function assincrona

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 import fs from 'fs';
 import chalk from "chalk";
 
-const trataErro = (erro) => {
+function trataErro (erro) {
     console.log(erro)
     throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório'));
 }
 
-const pegaArquivo = (caminhoDoArquivo) => {
-    const encoding = "UTF-8"
-        fs.promises
-            .readFile(caminhoDoArquivo,encoding)
-            .then((texto) => console.log(chalk.green(texto)))
-            .catch((erro) => trataErro(erro))
+async function pegaArquivo (caminhoDoArquivo) {
+    try {
+        const encoding = "UTF-8";
+        const textoResposta = await fs.promises.readFile(caminhoDoArquivo,encoding)
+        console.log(chalk.green(textoResposta));
+    } catch(erro) {
+        trataErro(erro)
+       
+    }
+    
 }
 
-// const pegaArquivo = (caminho) => {
-//     const encode = 'UTF-8';
-//     fs.readFile(caminho,encode,(erro,texto) => {
-//         if(erro) {
-//             return trataErro(erro);
-//         }
-//         console.log(chalk.green(texto));
-//     })
+//async await
 
+// const pegaArquivo = (caminhoDoArquivo) => {
+//     const encoding = "UTF-8"
+//         fs.promises
+//             .readFile(caminhoDoArquivo,encoding)
+//             .then((texto) => console.log(chalk.green(texto)))
+//             .catch((erro) => trataErro(erro))
 // }
 
+
  pegaArquivo('./arquivos/texto.md');
+
+
+ 
+
