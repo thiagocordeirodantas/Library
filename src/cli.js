f2459662cc5b07e45fbf528b3fe3545acd651c54
Thiago Cordeiro <thiagoccordeirodantass@gmail.com>
2023-05-08 17:52:49 -0300

    tratando novos erros

--- src/cli.js
+++ src/cli.js
@@ -2,20 +2,35 @@ import chalk from "chalk";
 import fs from 'fs';
 import pegaArquivo from "./index.js";
 
+function imprime(resultado,identificador = ''){
+    console.log(resultado,identificador)
+}
 
 const caminho = process.argv;
 
 async function processaTexto(argumentos){
+
+    try {
+        fs.lstatSync(caminho);  
+    } catch(erro) {
+        if(erro.code === 'ENOENT'){
+            console.log('arquivo ou diretorio nao existe')
+            return
+        }
+    }
+
+
+
     const caminho = argumentos[2];
 
     if(fs.lstatSync(caminho).isFile()){
         const resultado = await pegaArquivo(caminho);
-        console.log(resultado)
+       imprime(resultado)
     } else if (fs.lstatSync(caminho).isDirectory()) {
         const arquivos = await fs.promises.readdir(caminho)
         arquivos.forEach(async (nomeDeArquivo) => {
             const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
-            console.log(lista)
+            imprime(lista,nomeDeArquivo)
         })
     }
 }
